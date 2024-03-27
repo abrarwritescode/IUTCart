3a5be32fc47af8e9dc7c14eea6ba00586260a20c
abrarwritescode <146115506+abrarwritescode@users.noreply.github.com>
2024-03-27 19:33:30 +0600

    Added project files

---
+++ backend/data.js
@@ -0,0 +1,73 @@
+import bcrypt from 'bcryptjs';
+
+const data = {
+  users: [
+    {
+      name: 'Shoyeb',
+      email: 'mdsoyeb@iut-dhaka.edu',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Abul',
+      email: 'abul@gmail.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
+  products: [
+    {
+      // _id: '1',
+      name: 'RedHeaven Tshirt',
+      slug: 'redheaven-tshirt',
+      category: 'Tshirt',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStock: 0,
+      brand: 'ArtBoard',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High Quality Tshirt with Red Heaven Illustration.',
+    },
+    {
+      // _id: '2',
+      name: 'IUT Cat Tshirt',
+      slug: 'iut-cat-tshirt',
+      category: 'Tshirt',
+      image: '/images/p2.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'ArtBoard',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High Quality Tshirt with Illustration.',
+    },
+    {
+      // _id: '3',
+      name: 'IUT Tshirt',
+      slug: 'iut-tshirt',
+      category: 'Tshirt',
+      image: '/images/p3.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'ArtBoard',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High Quality Tshirt with Illustration.',
+    },
+    {
+      // _id: '4',
+      name: 'Tshirt',
+      slug: 'tshirt',
+      category: 'Tshirt',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'ArtBoard',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High Quality Tshirt with Illustration.',
+    },
+  ],
+};
+export default data;
